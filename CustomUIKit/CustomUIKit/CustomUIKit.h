e70b0d6381d917bcec3168db8017eaa08631f456
JAGELOO <jageloopwg@gmail.com>
2020-05-15 01:15:02 +0530

    empty project created

---
+++ CustomUIKit/CustomUIKit/CustomUIKit.h
@@ -0,0 +1,19 @@
+//
+//  CustomUIKit.h
+//  CustomUIKit
+//
+//  Created by Jageloo Yadav on 15/05/20.
+//  Copyright © 2020 Jageloo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomUIKit.
+FOUNDATION_EXPORT double CustomUIKitVersionNumber;
+
+//! Project version string for CustomUIKit.
+FOUNDATION_EXPORT const unsigned char CustomUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomUIKit/PublicHeader.h>
+
+
