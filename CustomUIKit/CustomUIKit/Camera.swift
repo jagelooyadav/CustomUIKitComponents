539188994cce24580820d006be1bdb16bb7fcd90
JAGELOO <jageloopwg@gmail.com>
2020-06-28 19:33:24 +0530

    volume level indetified

--- CustomUIKit/CustomUIKit/Camera.swift
+++ CustomUIKit/CustomUIKit/Camera.swift
@@ -33,7 +33,6 @@ public class Camera: NSObject {
     public var audioRecordingCompletionBlock:((Bool) -> Void)?
     
     public var isOnlyAudioRecord: Bool = false
-    var audioAssetWritter: AVAssetWriter?
     
     var fileLocation: String?
 
@@ -261,13 +260,48 @@ extension Camera: AVCapturePhotoCaptureDelegate {
 
 extension Camera: AVCaptureAudioDataOutputSampleBufferDelegate {
     public func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        let channel: AVCaptureAudioChannel? = connection.audioChannels.first
-        let averagePowerLevel = channel?.averagePowerLevel ?? 0.0
+        var buffer: CMBlockBuffer? = nil
         
-        if averagePowerLevel > 0.0 {
-            self.completion(success: true)
-        } else {
-            print("averagePowerLevel: \(averagePowerLevel)")
+        // Needs to be initialized somehow, even if we take only the address
+        let convenianceBuffer = AudioBuffer(mNumberChannels: 1, mDataByteSize: 0, mData: nil)
+        var audioBufferList = AudioBufferList(mNumberBuffers: 1,
+                                              mBuffers: convenianceBuffer)
+        
+        CMSampleBufferGetAudioBufferListWithRetainedBlockBuffer(
+            sampleBuffer,
+            bufferListSizeNeededOut: nil,
+            bufferListOut: &audioBufferList,
+            bufferListSize: MemoryLayout<AudioBufferList>.size(ofValue: audioBufferList),
+            blockBufferAllocator: nil,
+            blockBufferMemoryAllocator: nil,
+            flags: UInt32(kCMSampleBufferFlag_AudioBufferList_Assure16ByteAlignment),
+            blockBufferOut: &buffer
+        )
+        
+        let abl = UnsafeMutableAudioBufferListPointer(&audioBufferList)
+        
+        for buffer in abl {
+            let originRawPtr = buffer.mData
+            let ptrDataSize = Int(buffer.mDataByteSize)
+            
+            // From raw pointer to typed Int16 pointer
+            let buffPtrInt16 = originRawPtr?.bindMemory(to: Int16.self, capacity: ptrDataSize)
+            
+            // From pointer typed Int16 to pointer of [Int16]
+            // So we can iterate on it simply
+            let unsafePtrByteSize = ptrDataSize/Int16.bitWidth
+            let samples = UnsafeMutableBufferPointer<Int16>(start: buffPtrInt16,
+                                                            count: unsafePtrByteSize)
+            
+            // Average of each sample squared, then root squared
+            let sumOfSquaredSamples = samples.map(Float.init).reduce(0) { $0 + $1*$1 }
+            let averageOfSomething = sqrt(sumOfSquaredSamples / Float(samples.count))
+            
+            DispatchQueue.main.async {
+                if averageOfSomething > 10000.0 {
+                    self.completion(success: true)
+                }
+            }
         }
     }
 }
