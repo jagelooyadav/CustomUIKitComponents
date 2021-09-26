9fe953e68fd11fe4c7d43db1339540f33d067f7b
Mithilesh Kumar <mithilesh.kmr786@gmail.com>
2021-09-26 17:01:27 +0530

    Fixesalignment

--- CustomUIKit/CustomUIKit/ProductCardView.swift
+++ CustomUIKit/CustomUIKit/ProductCardView.swift
@@ -60,7 +60,8 @@ public class ProductCardView: ViewControl {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.numberOfLines = 0
-        label.font = UIFont.boldHeading
+        label.textAlignment = .left
+        label.font = UIFont.boldBody
         return label
     }()
     
@@ -75,7 +76,8 @@ public class ProductCardView: ViewControl {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.numberOfLines = 0
-        label.font = UIFont.heading
+        label.font = UIFont.boldHeading
+        
         return label
     }()
     
@@ -83,8 +85,8 @@ public class ProductCardView: ViewControl {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.numberOfLines = 0
-        label.font = UIFont.body
-        label.textColor = Color.darkGrey3Colour
+        label.font = UIFont.boldSFRegualarSubHeading
+        label.textColor = Color.grey1Colour
         return label
     }()
     
@@ -92,8 +94,8 @@ public class ProductCardView: ViewControl {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.numberOfLines = 0
-        label.font = UIFont.body
-        label.textColor = Color.darkGrey3Colour
+        label.font = UIFont.boldSFRegualarSubHeading
+        label.textColor = Color.darkGrey1Colour
         return label
     }()
     
@@ -109,32 +111,36 @@ public class ProductCardView: ViewControl {
     
     private func setup() {
         self.backgroundColor = .clear
-        
         let container = ContainerView()
-        self.backgroundColor = .clear
         self.addSubview(container)
         container.anchorToSuperView(leading: 16.0, trailing: 16.0)
         
-       
+        let titleContainer = UIView()
+        titleContainer.addSubview(productinfoLabel)
+        productinfoLabel.anchorToSuperView(trailingRelation: .greaterOrEqual, leading: 16, trailing: 16, top: 16, bottom: 16)
         let stack = UIStackView()
         stack.axis = .vertical
         stack.spacing = 20.0
+        stack.distribution = .fill
         stack.alignment = .center
-        //stack.distribution = .equalSpacing
         container.addSubview(stack)
         stack.anchorToSuperView(leading: 16, trailing: 16, top: 16, bottom: 16)
-       
+    
         let  devider = DividerView()
-        devider.backgroundColor = Color.grey1Colour
+        devider.backgroundColor = Color.grey3Colour
         devider.translatesAutoresizingMaskIntoConstraints = false
-        stack.addArrangedSubview(productinfoLabel)
+        stack.addArrangedSubview(titleContainer)
         stack.addArrangedSubview(productImageView)
         stack.addArrangedSubview(producttitleLabel)
         stack.addArrangedSubview(productsubtitleLabel)
         stack.addArrangedSubview(devider)
-        devider.widthAnchor.constraint(equalTo: stack.widthAnchor).isActive = true
+        
+        devider.widthAnchor.constraint(equalTo: productsubtitleLabel.widthAnchor, constant: 120).isActive = true
         stack.addArrangedSubview(productpriceLabel)
-
+        stack.setCustomSpacing(10, after: productsubtitleLabel)
+        stack.setCustomSpacing(10, after: devider)
+        
+        titleContainer.widthAnchor.constraint(equalTo: stack.widthAnchor, constant: 20).isActive = true
         
     }
 }
