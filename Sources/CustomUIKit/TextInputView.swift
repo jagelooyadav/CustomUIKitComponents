9e766438730788b856a304fc6aaaf1472e875b28
Jageloo <jageloo.yadav@gmail.com>
2021-12-29 21:56:55 +0530

    Removed unwanted documentation

--- Sources/CustomUIKit/TextInputView.swift
+++ Sources/CustomUIKit/TextInputView.swift
@@ -144,9 +144,6 @@ public class TextInputView: InputView {
         }
     }
 
-    // MARK: Overrides
-    
-    /// The value within the text field.
     override public var value: String? {
         get {
             return self.textField.text
@@ -162,20 +159,10 @@ public class TextInputView: InputView {
         return self.textField
     }
     
-    /**
-     Notifies the receiver that it is about to become first responder in its window.
-     
-     - returns: `true` if the receiver accepts first-responder status or `false` if it refuses this status.
-     */
     override public func becomeFirstResponder() -> Bool {
         return self.textField.becomeFirstResponder()
     }
-    
-    /**
-     Notifies the text field that it has been asked to relinquish its status as first responder in its window.
-     
-     - returns: `true` if the text field is the first responder, `false`.
-     */
+   
     @discardableResult override public func resignFirstResponder() -> Bool {
         return self.textField.resignFirstResponder()
     }
@@ -220,14 +207,6 @@ public class TextInputView: InputView {
         return textField
     }()
     
-    // MARK: - Functions
-    
-    /**
-     Insert text at the index corresponding to the cursor.
-     The delegate `inputViewTextDidChange:` will get colled after inserting text into the texfield.
-     
-     - parameter text: The text to be inserted in to TextField.
-     */
     @objc func insertTextAtCurrentPostion(text: String) {
         self.textField.insertText(text)
     }
@@ -328,88 +307,20 @@ extension TextInputView: UITextFieldDelegate {
 }
 
 /// A delegate to observe changes within `TextInputView`.
-@objc public protocol TextInputViewDelegate: class {
+@objc public protocol TextInputViewDelegate: AnyObject {
+        @objc optional func inputViewShouldBeginEditing(inInputView inputView: TextInputView) -> Bool
     
-    /**
-     Called to perform a validation of the text field and determine if the input is valid or not.
-     
-     If this method is not implemented by the delegate, then validation states will not be displayed.
-     
-     - parameter inputView: The input view from where the delegate call originated.
-     
-     - returns: `true` if the input is valid, else `false`.
-     */
-    //@objc optional func shouldDisplayValidState(inInputView inputView: TextInputView) -> Bool
-    
-    /**
-     Asks the delegate if editing should begin in the specified text input view.
-     
-     Implement this method if you want to prevent editing from happening in some situations.
-     
-     If you do not implement this method, the text input view acts as if this method had returned true.
-     
-     - parameter inputView: The input view from where the delegate call originated.
-     
-     - returns: `true` if editing should begin or `false` if it should not.
-     */
-    @objc optional func inputViewShouldBeginEditing(inInputView inputView: TextInputView) -> Bool
-    
-    /**
-     Tells the delegate that editing began in the specified text input view.
-     
-     This method notifies the delegate that the specified text input view just became the first responder.
-     
-     Implementation of this method by the delegate is optional.
-     
-     - parameter inputView: The input view from where the delegate call originated.
-     */
     @objc optional func inputViewDidBeginEditing(inInputView inputView: TextInputView)
     
-    /**
-     Asks the delegate if editing should stop in the specified text input view.
-     
-     - parameter inputView: The input view from where the delegate call originated.
-     
-     - returns: `true` if editing should stop or `false` if it should continue.
-     */
     @objc optional func inputViewShouldEndEditing(inInputView inputView: TextInputView) -> Bool
     
-    /**
-     Tells the delegate that editing stopped for the specified text input view.
-     
-     - parameter inputView: The input view from where the delegate call originated.
-     */
     @objc optional func inputViewDidEndEditing(inInputView inputView: TextInputView)
     
-    /**
-     Asks the delegate if the specified text should be changed.
-     
-     - parameter inputView: The input view from where the delegate call originated.
-     
-     - returns: `true` if the specified text range should be replaced; otherwise, `false` to keep the old text.
-     */
     @objc optional func inputView(inInputView inputView: TextInputView, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool
-    
-    /**
-     Asks the delegate if the text input view should process the pressing of the return button.
-     
-     - parameter inputView: The input view from where the delegate call originated.
-     
-     - returns: `true` if the text field should implement its default behavior for the return button; otherwise, `false`.
-     */
+  
     @objc optional func inputViewShouldReturn(inInputView inputView: TextInputView) -> Bool
     
-    /**
-     Tells the delegate that editing changed for the input view.
-     
-     - parameter inputView: The input view from where the delegate call originated.
-     */
     @objc optional func inputViewTextDidChange(inInputView inputView: TextInputView)
     
-    /**
-     Asks the delegate if the input view’s current text should be removed. Called when clear button pressed.
-     
-     - parameter inputView: The input view from where the delegate call originated.
-     */
     @objc optional func inputViewShouldClear(inInputView inputView: TextInputView) -> Bool
 }
