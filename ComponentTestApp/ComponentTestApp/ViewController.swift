419c8da2c577233d680f921e7ee0827d64f6dd0c
Jageloo <jageloo.yadav@gmail.com>
2022-01-02 10:00:48 +0530

    Added test app for custom UI Kit components

--- ComponentTestApp/ComponentTestApp/ViewController.swift
+++ ComponentTestApp/ComponentTestApp/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let button = Button()
         // Do any additional setup after loading the view.
     }
 }
