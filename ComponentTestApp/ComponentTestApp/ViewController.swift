6972f0e6ce7a70d4629b4ded3d3c3ef3544c9050
Jageloo <jageloo.yadav@gmail.com>
2022-01-02 12:16:07 +0530

    Added buttons sample in test app

--- ComponentTestApp/ComponentTestApp/ViewController.swift
+++ ComponentTestApp/ComponentTestApp/ViewController.swift
@@ -8,21 +8,46 @@
 import UIKit
 import CustomUIKit
 
-class ViewController: UIViewController {
+class ViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let button = Button(type: .custom)
-        self.view.addSubview(button)
-        button.setTitle("Custom button", for: .normal)
-        button.customType = .plane
-        button.anchorToSuperView(topAnchor: view.safeAreaLayoutGuide.topAnchor,
-                                 bottomRelation: .ignore,
+        self.title = "Custom Componets"
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 10
+        self.scrollingContentView.addSubview(stack)
+        stack.anchorToSuperView(topAnchor: view.safeAreaLayoutGuide.topAnchor,
+                                 bottomRelation: .greaterOrEqual,
                                  leading: 20.0,
                                  trailing: 20.0,
-                                 top: 20.0)
-        button.primaryColor = .blue
-        button.customType  = .bordered
+                                 top: 0)
+        
+        let label = UILabel()
+        label.text = "Buttons"
+    
+        stack.addArrangedSubview(label)
+        stack.addArrangedSubview(createButton(type: .bordered, color: .blue))
+        stack.addArrangedSubview(createButton(type: .plane, color: .blue))
+        
+        stack.addArrangedSubview(createButton(type: .bordered, color: Color.darkRed))
+        stack.addArrangedSubview(createButton(type: .plane, color: Color.darkRed))
+        
+        stack.addArrangedSubview(createButton(type: .bordered, color: .yellow))
+        stack.addArrangedSubview(createButton(type: .plane, color: .yellow))
+    }
+    
+    func createButton(type: Button.ButtonType, color: UIColor) -> Button {
+        let button1 = Button()
+        button1.customType = type
+        button1.primaryColor = color
+        let title = type == .bordered ? "Bordered button" : "Plane Button"
+        button1.setTitle(title, for: .normal)
+        return button1
+    }
+    
+    override var shouldEmbedInScrollView: Bool {
+        return true
     }
 }
 
---
+++ Sources/CustomUIKit/BaseViewController.swift
@@ -0,0 +1,52 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jageloo Yadav on 02/01/22.
+//
+
+import Foundation
+import UIKit
+
+open class BaseViewController: UIViewController {
+    
+    public var backgroundImage = UIImageView()
+    public let scrollingContentView = UIView()
+    
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if shouldEmbedInScrollView {
+            let wrapperView = UIView()
+            let scrollView = UIScrollView()
+            wrapperView.backgroundColor = .clear
+            self.view.addSubview(wrapperView)
+            wrapperView.anchorToSuperView(topAnchor: view.safeAreaLayoutGuide.topAnchor, bottomAnchor: view.safeAreaLayoutGuide.bottomAnchor)
+            scrollView.backgroundColor = .clear
+            wrapperView.addSubview(scrollView)
+            scrollView.anchorToSuperView()
+            scrollingContentView.backgroundColor = .clear
+            scrollView.addSubview(scrollingContentView)
+            scrollingContentView.anchorToSuperView()
+            scrollingContentView.widthAnchor.constraint(equalTo: wrapperView.widthAnchor).isActive = true
+            scrollingContentView.heightAnchor.constraint(greaterThanOrEqualTo: wrapperView.heightAnchor).isActive = true
+        }
+        self.view.addSubview(self.backgroundImage, insets: .zero, ignoreConstant: .bottom)
+    
+        self.view.sendSubviewToBack(self.backgroundImage)
+    }
+    
+    public override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    open var shouldEmbedInScrollView: Bool {
+        return false
+    }
+    
+    @objc func back() {
+        self.navigationController?.popViewController(animated: true)
+    }
+}
+
+
